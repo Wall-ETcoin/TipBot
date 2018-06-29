14ed6953fc2dcf5a6aa4389be445338532e8aec6
Wall-ETcoin <git-w@ll-et.com>
2018-06-29 21:18:06 +0000

    Fix formatting of the code

--- bot/modules/helpTipper.js
+++ bot/modules/helpTipper.js
@@ -9,11 +9,11 @@ let phoenixFee = config.get('slc').paytxfee;
 //let featherFee = config.get('ftc').paytxfee;
 exports.commands = ['tiphelp'];
 exports.tiphelp = {
-  usage: '<subcommand>',
-  description: 'This commands has been changed to currency specific commands!',
-  process: function(bot, message) {
-    message.author.send(
-      '__**SuperLumic coin (SLC) Tipper**__\nTransaction Fees: **' + phoenixFee + '**\n    **!tipslc balance** : get your balance\n    **!tipslc deposit** : get address for your deposits\n    **!tipslc withdraw <ADDRESS> <AMOUNT>** : withdraw coins to specified address\n    **!tipslc <@user> <amount>** :mention a user with @ and then the amount to tip them\n    **!tipslc private <user> <amount>** : put private before Mentioning a user to tip them privately.\n'
-    );
-  }
+    usage: '<subcommand>',
+    description: 'This commands has been changed to currency specific commands!',
+    process: function(bot, message) {
+        message.author.send(
+                '__**SuperLumic coin (SLC) Tipper**__\nTransaction Fees: **' + phoenixFee + '**\n    **!tipslc balance** : get your balance\n    **!tipslc deposit** : get address for your deposits\n    **!tipslc withdraw <ADDRESS> <AMOUNT>** : withdraw coins to specified address\n    **!tipslc <@user> <amount>** :mention a user with @ and then the amount to tip them\n    **!tipslc private <user> <amount>** : put private before Mentioning a user to tip them privately.\n'
+                );
+    }
 };
